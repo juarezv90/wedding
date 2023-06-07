e16852f6e214a3e476ad176153e86242dbd261ab
Victor Juarez <juarezv1990@gmail.com>
2023-06-07 16:49:32 -0400

    initial push

---
+++ scripts/dateCountdown.js
@@ -0,0 +1,8 @@
+const cntDown = document.getElementById('weddingCntDown')
+
+const proposedDate = new Date('2024.04.19');
+const today = new Date();
+
+const diff = Math.trunc(Math.abs((proposedDate - today))/1000/60/60/24);
+
+cntDown.innerHTML = `${diff} days left!`
