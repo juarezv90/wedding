128e52571e554dda287256ceed364f0ce5f906df
Victor Juarez <juarezv1990@gmail.com>
2023-06-08 13:54:13 -0400

    Fixed several Perfomance and SEO issues

--- scripts/dateCountdown.js
+++
@@ -1,8 +0,0 @@
-const cntDown = document.getElementById('weddingCntDown')
-
-const proposedDate = new Date('2024.04.19');
-const today = new Date();
-
-const diff = Math.trunc(Math.abs((proposedDate - today))/1000/60/60/24);
-
-cntDown.innerHTML = `${diff} days left!`
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-import './dateCountdown.js'
+function countDown() {
+    const cntDown = document.getElementById("weddingCntDown");
+  
+    const proposedDate = new Date("2024.04.19");
+    const today = new Date();
+  
+    const diff = Math.trunc(Math.abs(proposedDate - today) / 1000 / 60 / 60 / 24);
+  
+    cntDown.innerHTML = `${diff} days left!`;
+  }
+  
+  countDown();
